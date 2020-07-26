b41f00bd19532bb2b5a5034aa788c4e7a20ecb78
Ekram <ekremkose2001@gmail.com>
2020-07-25 22:40:16 -0500

    yeni classdan

---
+++ src/yeni_proje/YeniClass.java
@@ -0,0 +1,10 @@
+package yeni_proje;
+
+public class YeniClass {
+
+	public static void main(String[] args) {
+		System.out.println("Ilk commit local master den");
+
+	}
+
+}
---
+++ src/yeni_proje/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author ekrem
+ *
+ */
+package yeni_proje;
