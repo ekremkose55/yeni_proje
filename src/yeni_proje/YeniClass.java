63720a3ed39bdc0c53aefe78aadcc4680d454d01
ekremkose55 <67019372+ekremkose55@users.noreply.github.com>
2020-07-26 00:06:01 -0500

    Update YeniClass.java
    feature ilk commit

--- src/yeni_proje/YeniClass.java
+++ src/yeni_proje/YeniClass.java
@@ -4,6 +4,7 @@ public class YeniClass {
 
 	public static void main(String[] args) {
 		System.out.println("Ilk commit local master den");
+		System.out.println("Ikinci commit remote feature'den");
 
 	}
 
